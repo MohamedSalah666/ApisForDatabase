5393c958ae7228ae44356f2e97d13b1d3288a8bc
Mohamed Salah <132882633+MohamedSalah666@users.noreply.github.com>
2026-05-17 16:29:26 +0300

    Add files via upload

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,1 +1,8 @@
-
+<?php
+$conn = oci_connect('ECommerceproj', 'Projectfager', 'localhost/XEPDB1');
+if (!$conn) {
+    $e = oci_error();
+    echo json_encode(["error" => $e['message']]);
+    exit;
+}
+?>
